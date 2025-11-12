9e19071254f4c10cba4375297f18923688a745b8
esegev1 <esegev1@gmail.com>
2025-11-12 16:40:58 -0500

    services added

---
+++ src/components/CreatePet.jsx
@@ -0,0 +1,23 @@
+function CreatePet() {
+
+
+  return (
+    <>
+      <h1>Create a new pet:</h1>
+
+      <form onSubmit="">
+        <label htmlFor="">Name: </label>
+        <input type="text" name="" />
+
+        <label htmlFor="">Breed: </label>
+        <input type="text" name="" />
+
+        <label htmlFor=""> Age: </label>
+        <input type="text" name="" />
+
+      </form>
+    </>
+  )
+}
+
+export default CreatePet
---
+++ src/services/petsService.js
@@ -0,0 +1,12 @@
+const BASE_URL = `localhost:3000`;
+
+const index = async (searchString) => {
+    try {
+        //code here
+
+    } catch (error) {
+        console.error(`Error fetching starships: ${error}`)
+    }
+}
+
+export { index }
